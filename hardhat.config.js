129105f5a725a3e1d4cec8fe3b15fa11997d1ab9
Spark <sparkhuang@nine-yi.com>
2022-04-24 23:58:33 +0800

    fix: setting

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,27 +10,34 @@ const EtherScan_PRIVATE_KEY = "要填寫";
 const INFURA_API_KEY = "要填寫"
 
 module.exports = {
-  solidity: "0.5.16",
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 2000
+
+  solidity: {
+    version: "0.5.16",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
     }
   },
   defaultNetwork: 'localhost',
   networks: {
     hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+        blockNumber: 14646872
+      },
       allowUnlimitedContractSize: true
     },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [`${Rinkeby_PRIVATE_KEY}`]
+      accounts: [`${Rinkeby_PRIVATE_KEY}`],
+      allowUnlimitedContractSize: true
     },
     localhost: {
       url: 'http://localhost:8545',
       allowUnlimitedContractSize: true
     },
-    
   },
   etherscan: {
     apiKey: EtherScan_PRIVATE_KEY
